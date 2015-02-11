44ed427999b7790aaeac0b67e6375df3228e8c55
phil <uniphil@gmail.com>
2015-02-11 11:17:25 -0500

    add project location bubbles
    fixes #41

--- client/scripts/components/country.jsx
+++ client/scripts/components/country.jsx
@@ -3,10 +3,22 @@
 var React = require('react');
 var Reflux = require('reflux');
 var TopoLayer = require('./map/topoLayer.jsx');
+var PointLayer = require('./map/pointLayer.jsx');
 var CountryActions = require('../actions/countryActions');
 var MapViewActions = require('../actions/mapViewActions');
 var MetaActions = require('../actions/pageMetaActions');
 var CountryStore = require('../stores/countryStore');
+
+
+function locationsToMultiPoint(locations) {
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'MultiPoint',
+      coordinates: locations
+    }
+  };
+}
 
 
 module.exports  = React.createClass({
@@ -25,6 +37,17 @@ module.exports  = React.createClass({
     }
   },
 
+  locationPin: function(featureData, latlng) {
+    return L.marker(latlng, {
+      icon: L.divIcon({
+        className: 'project-location-marker',
+        iconSize: [16, 16],
+        iconAnchor: [8, 8]
+      }),
+      riseOnHover: true
+    });
+  },
+
   styleTopoLayer: function(layer) {
     return {
       color: 'hsl(220, 100%, 10%)',
@@ -39,22 +62,36 @@ module.exports  = React.createClass({
     var thisCountry = CountryStore.getCountry(this.props.params.countryId),
         hasLoaded = !!thisCountry;  // cast to bool, false if thisCountry is undefined
 
-    // update the app title. TODO: research title-managing libs
-    if (hasLoaded) {
-      MetaActions.setTitle(thisCountry.properties.name);
-      MapViewActions.changeBounds(thisCountry.properties.bounds, {debounceKey: 'country' + thisCountry.id});
+    if (!hasLoaded) {
+      MetaActions.setTitle('(loading country...)');
+      return <div className="hidden"></div>;
+    }
 
+    MetaActions.setTitle(thisCountry.properties.name);
+    MapViewActions.changeBounds(thisCountry.properties.bounds, {debounceKey: 'country' + thisCountry.id});
+
+    var projectSites = thisCountry.properties.projects.map(function(project, i) {
+      // This loop could potentially become a performance bottleneck.
+      // If it does, we can put all the projects on one layer and one component.
+      // Maybe a big FeatureCollection of MultiPoints?
       return (
+        <PointLayer
+          key={i}
+          getMap={this.props.getMap}
+          pin={this.locationPin}
+          geojson={locationsToMultiPoint(project.locations)} />
+      );
+    }, this);
+
+    return (
+      <div className="hidden">
         <TopoLayer
           getMap={this.props.getMap}
           style={this.styleTopoLayer}
           topojson={thisCountry.properties.topojson} />
-      );
-
-    } else {
-      MetaActions.setTitle('(loading country...)');
-      return <div className="hidden"></div>
-    }
+        {projectSites}
+      </div>
+    );
   }
 
 });
