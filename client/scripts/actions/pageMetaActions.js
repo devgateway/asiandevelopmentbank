6638b02e09b265e4846aa11911227182f3600239
phil <uniphil@gmail.com>
2015-02-18 18:10:56 -0500

    sketchy demo

--- client/scripts/actions/pageMetaActions.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Reflux = require('reflux');
-
-
-module.exports = Reflux.createActions([
-  'setTitle',
-  'transitionTo',  // pageMetaStore just proxies this to react-router
-]);
--- client/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var React = require('react');
-var router = require('./router.jsx').get();  // function indirection to avoid circular import issues
-
-
-router.run(function(Handler, state) {
-  React.render(React.createElement(Handler, state), document.getElementById('app-wrapper'));
-});
